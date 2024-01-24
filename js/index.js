b0c507b1e034598eb23b26a50abc02c4072822ed
Xuan <127477394+xuanlin129@users.noreply.github.com>
2024-01-24 16:04:46 +0800

    new post

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('.slick_01').slick({
+  // 自動播放
+  autoplay: true,
+  autoplaySpeed: 2500, // default: 3000
+  // 箭頭按鈕
+  arrows: true, // default: true
+  dots: true,
+  // 顯示數量
+  slidesToShow: 1,
+  // 輪播數量/次
+  slidesToScroll: 1,
+});
